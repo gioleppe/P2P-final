e7232894592fe40061fd1486633743e0603f1fd9
gotoxy <tommasocolella95@gmail.com>
2021-05-22 00:25:16 +0200

    add envelope casting test

--- test/mayor_test.js
+++ test/mayor_test.js
@@ -1,18 +1,31 @@
 const Mayor = artifacts.require("Mayor");
 const ethers = require("ethers");
 
-contract("Mayor", accounts => {
+contract("Mayor", async accounts => {
     it("Should be deployed", () => {
         Mayor.deployed();
     });
 
     it("Should correctly compute envelope", () => {
         // precompute the envelope and automatically encode it
-        let _envelope = ethers.utils.solidityKeccak256([ 'uint', 'bool', 'uint' ], [ 1, true, 1 ]);
+        let _envelope = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(["uint", "bool", "uint"], [1, true, 1]));
 
-        // check if the 
-        Mayor.deployed()
+        // check if the envelope returned by compute envelope is the same as the one we computed before
+        return Mayor.deployed()
         .then(instance => instance.compute_envelope.call(1, true, 1))
         .then(envelope => assert.equal(envelope, _envelope));
     });
+
+    it("Should correctly cast envelopes", async () => {
+
+        let envelope = await Mayor.deployed()
+        .then(instance => instance.compute_envelope.call(1, true, 1));
+
+        // console.log(envelope);
+        
+        return Mayor.deployed()
+        .then(instance => instance.cast_envelope.call(envelope));
+
+    })
+
 })
